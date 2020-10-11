d2ab888a76a2ff0193760ef7bb6a71312a402941
Bernard Adhitya <bernard.adhitya@gmail.com>
2020-10-11 16:05:38 +0700

    [Bernard] Create styling for tab

---
+++ src/Components/NavPane/AntTab.js
@@ -0,0 +1,38 @@
+import { withStyles } from "@material-ui/core/styles";
+import { Tab } from "@material-ui/core";
+import React from "react";
+
+const AntTab = withStyles((theme) => ({
+  root: {
+    textTransform: 'none',
+    minWidth: 72,
+    fontWeight: theme.typography.fontWeightRegular,
+    marginRight: theme.spacing(4),
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+      color: '#40a9ff',
+      opacity: 1,
+    },
+    '&$selected': {
+      color: '#1890ff',
+      fontWeight: theme.typography.fontWeightMedium,
+    },
+    '&:focus': {
+      color: '#40a9ff',
+    },
+  },
+  selected: {},
+}))((props) => <Tab disableRipple {...props} />);
+
+export default AntTab;
