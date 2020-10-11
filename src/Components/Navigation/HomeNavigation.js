3d75616a98909a5d4c9e5215dfacebbc24c66e45
Bernard Adhitya <bernard.adhitya@gmail.com>
2020-10-11 16:05:16 +0700

    [Bernard] Add routing and navigation with react-router-dom

---
+++ src/Components/Navigation/HomeNavigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
+import Home from '../../Containers/Home/Home';
+
+
+const HomeNavigation = () => {
+  return (
+    <Switch>
+      <Route exact path='/'><Redirect to='/home'/></Route>
+      <PrivateRoute enableNavPane path='/home' component={Home}/>
+    </Switch>
+  );
+};
+
+export default HomeNavigation;
---
+++ src/Containers/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      Hello World
+    </div>
+  );
+};
+
+Home.propTypes = {
+
+};
+
+export default Home;
